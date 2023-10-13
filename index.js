eb5a44d39b316a84800b8fdb46edf6aa8dc719fe
ahmed-abdi1991 <squarehajji@gmail.com>
2023-10-13 11:09:57 +0300

    displaying weather function

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 let weather = {
   "apikey": "38732f8a80baa1076a6124eaa353a557",
-  fetchWeather: function () {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=Nairobi&units=metric&appid=38732f8a80baa1076a6124eaa353a557"
+  fetchWeather: function (city) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
+    + city 
+    + "&units=metric&appid=" 
+    + this.apikey
     ).then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => this.displayweather(data));
+  },
+  displayweather: function(data){
+    const { name } = data;
+    const { icon,description } = data.weather[0];
+    const { temp,humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name,icon,description,temp,humidity,speed) 
   }
-}
+};
