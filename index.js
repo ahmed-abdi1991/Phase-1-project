a208c2e4419ddea7ed5d71c248d617b11c229096
ahmed-abdi1991 <squarehajji@gmail.com>
2023-10-13 10:51:45 +0300

    fetching

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 let weather = {
-  "apikey":"38732f8a80baa1076a6124eaa353a557"
+  "apikey": "38732f8a80baa1076a6124eaa353a557",
+  fetchWeather: function () {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=Nairobi&units=metric&appid=38732f8a80baa1076a6124eaa353a557"
+    ).then((response) => response.json())
+    .then((data) => console.log(data));
+  }
 }
